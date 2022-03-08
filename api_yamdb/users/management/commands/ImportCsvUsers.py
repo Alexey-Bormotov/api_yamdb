7a70d8304c5bf598899b693ce82d9a6d1d9d647e
UfoNexus <51679952+UfoNexus@users.noreply.github.com>
2022-03-08 10:33:06 +0300

    csv import for users

---
+++ api_yamdb/users/management/commands/ImportCsvUsers.py
@@ -0,0 +1,28 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Импорт данных из csv в модель Users'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str, help='Путь к файлу')
+
+    def handle(self, *args, **options):
+        file_path = options['path']
+        with open(file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+            for row in reader:
+                User.objects.create(
+                    id=row[0],
+                    username=row[1],
+                    email=row[2],
+                    role=row[3],
+                    bio=row[4],
+                    first_name=row[5],
+                    last_name=row[6]
+                )
